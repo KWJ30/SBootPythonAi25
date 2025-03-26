f215f3ed8fd6109cbd3cfb91a60c993dba706e8d
312-05\gidtjrwns <gidtjrwns30@naver.com>
2025-03-26 13:14:22 +0900

    자바 파이썬 연동

---
+++ www/src/main/java/com/mbcAI/www/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.mbcAI.www.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }   // localhost 접속시 index.html 반환
+
+}
